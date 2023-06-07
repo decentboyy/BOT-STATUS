9e2f76dc4d16f8f7f3d486d01a9992d90d80d054
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-06-07 22:15:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,14 +49,30 @@ async def main():
                         TEXT += f"\n\n**🤖-[{ok.first_name}](tg://openmessage?user_id={ok.id}): {msg}**"
                         await app.read_chat_history(bots)
                 except FloodWait as e:
-                    await asyncio.sleep(e.value)
-            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-            date = time.strftime("%d %b %Y")
-            time = time.strftime("%I:%M: %p")
-            TEXT += f"\n\n--Last checked on--: \n{date}\n{time} ({TIME_ZONE})\n\n**Refreshes Automatically After Every 15 Min.**"
-            await bot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID,
-                                        TEXT)
-            await asyncio.sleep(900)
+                   async def system_status(update, context):
+    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    status = "<b>======[ SYSTEM INFO ]======</b>\n\n"
+    status += "<b>sʏsᴛᴇᴍ ᴜᴘᴛɪᴍᴇ:</b> <code>" + str(uptime) + "</code>\n"
 
+    uname = platform.uname()
+    status += "<b>sʏsᴛᴇᴍ:</b> <code>" + str(uname.system) + "</code>\n"
+    status += "<b>ɴᴏᴅᴇ ɴᴀᴍᴇ:</b> <code>" + str(uname.node) + "</code>\n"
+    status += "<b>ʀᴇʟᴇᴀsᴇ:</b> <code>" + str(uname.release) + "</code>\n"
+    status += "<b>ᴠᴇʀsɪᴏɴ:</b> <code>" + str(uname.version) + "</code>\n"
+    status += "<b>ᴍᴀᴄʜɪɴᴇ:</b> <code>" + str(uname.machine) + "</code>\n"
+    status += "<b>ᴘʀᴏᴄᴇssᴏʀ:</b> <code>" + str(uname.processor) + "</code>\n\n"
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
+    status += "<b>ᴄᴘᴜ ᴜsᴀɢᴇ:</b> <code>" + str(cpu) + " %</code>\n"
+    status += "<b>ʀᴀᴍ ᴜsᴀɢᴇ:</b> <code>" + str(mem[2]) + " %</code>\n"
+    status += "<b>sᴛᴏʀᴀɢᴇ ᴜsᴇᴅ:</b> <code>" + str(disk[3]) + " %</code>\n\n"
+    status += "<b>ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:</b> <code>" + python_version() + "</code>\n"
+    status += "<b>ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ:</b> <code>" + str(__version__) + "</code>\n"
+    await context.bot.sendMessage(
+        update.effective_chat.id, status, parse_mode=ParseMode.HTML
+    )
+SYS_STATUS_HANDLER = CommandHandler(["stats", "sysinfo"], system_status, block=False)
+rani.add_handler(SYS_STATUS_HANDLER)
 
 bot.run(main())
